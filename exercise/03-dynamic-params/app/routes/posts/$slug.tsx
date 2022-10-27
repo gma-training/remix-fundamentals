a9f399604ad231f3497a21903d0ada1d0b3840cf
Graham Ashton <graham@effectif.com>
2022-10-27 16:38:37 +0100

    Ex 3: Use tiny-invariant for type narrowing
    See https://www.npmjs.com/package/tiny-invariant for details.

--- exercise/03-dynamic-params/app/routes/posts/$slug.tsx
+++ exercise/03-dynamic-params/app/routes/posts/$slug.tsx
@@ -2,13 +2,14 @@ import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
+import invariant from "tiny-invariant";
 import { marked } from "marked";
 
 import { getPost } from "~/models/post.server";
 
 export async function loader({ params }: LoaderArgs) {
   const { slug } = params;
-  if (!slug) throw new Error("Slug not defined");
+  invariant(slug, "slug not set");
   const post = await getPost(slug);
   const html = marked(post.markdown);
   return json({ title: post.title, html });
