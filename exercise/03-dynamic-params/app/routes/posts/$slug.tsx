d33f68ee4f64bb811168fb48401a3f198c4e0db6
Graham Ashton <graham@effectif.com>
2022-10-27 16:29:03 +0100

    Ex 3: Display post markdown on posts/:slug
    Note that we're converting Markdown to HTML on the server, in the loader
    function, rather than in the client. That'll use more bandwidth, but the
    server feels like the right place to do it.
    
    Benefits of converting the Markdown on the server:
    
    - We don't need to send the marked library to the client
    - We're not asking low powered devices like mobiles to do the work of
      converting lots of Markdown to HTML

--- exercise/03-dynamic-params/app/routes/posts/$slug.tsx
+++ exercise/03-dynamic-params/app/routes/posts/$slug.tsx
@@ -2,20 +2,28 @@ import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
+import { marked } from "marked";
+
 import { getPost } from "~/models/post.server";
 
 export async function loader({ params }: LoaderArgs) {
   const { slug } = params;
   if (!slug) throw new Error("Slug not defined");
   const post = await getPost(slug);
-  return json({ post });
+  const html = marked(post.markdown);
+  return json({ title: post.title, html });
 }
 
 export default function Post() {
-  const { post } = useLoaderData<typeof loader>();
+  const { title, html } = useLoaderData<typeof loader>();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
+      <PostContent html={html} />
     </main>
   );
+}
+
+function PostContent({ html }: { html: string }) {
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
